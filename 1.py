e3aacc095c59c70ffa3f6ad7939d598e93a9dff5
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-09 12:01:05 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,10 +1,8 @@
 # диапазон [1,100]
 import math
-start = 1
-end = 100
 w = int(input())
 k=0
-for i in range(start, end+1):
+for i in range(1, n+1):
     if math.gcd(w, i) == 1:
        k+=1
 print(k)
