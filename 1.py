9163685add8d0100c0b0880729d8c547c89ccd06
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-09 12:13:11 +0300

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,13 +1,13 @@
-# диапазон [1,100]
+#1 задача
 import math
 w = int(input())
 k=0
-for i in range(1, n+1):
+for i in range(1, w+1):
     if math.gcd(w, i) == 1:
        k+=1
 print(k)
 
-
+#2 задача
 n=int(input())
 sum=0
 while n>0:
@@ -17,5 +17,20 @@ while n>0:
         sum+=k
     n=n//10
 print(sum)
+
+#3 задача
+import math
+w = int(input())
+sum=0
+n=w
+while n>0:
+    k=n%10
+    sum+=k
+    n=n//10
+for i in range(2, w):
+    if w%i==0:
+        math.gcd(sum, i) == 1
+        break
+print(i)
 
 
