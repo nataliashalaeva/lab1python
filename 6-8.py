cc1ce9f5caef07220f02b2167f272defe751fe29
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-16 11:57:23 +0300

    Update 6-8.py

--- 6-8.py
+++ 6-8.py
@@ -1,7 +1,19 @@
 #Задачи 2,10,17
 #2 Дана строка. Необходимо найти все строчные символы латиницы, которые в ней используются.
-  
+  def find(string):
+    letters = set()
+    for char in string:
+        if char.islower() and char.isalpha():
+            letters.add(char)
+    return letters
+    
 #10 Дана строка. Необходимо найти количество задействованных символов латиницы в этой строке (без дубликатов).
+def cou(string):
+  chars = set()
+  for char in string:
+    if char.isalpha() and char.isascii() and char.islower():
+     chars.add(char)
+  return len(chars)
 
 #17 Дана строка в которой записан путь к файлу. Необходимо найти имя файла без расширения.
 def file(path):
