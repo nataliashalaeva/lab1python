85d0982edf3ada3b4cd7e4ea0a7dfae3246a780d
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-16 12:02:26 +0300

    Update 6-8.py

--- 6-8.py
+++ 6-8.py
@@ -13,7 +13,7 @@ def cou(string):
    for char in string:
        if char.isalpha() and char.isascii() and char.islower():
           chars.add(char)
-  return len(chars)
+   return len(chars)
 
 #17 Дана строка в которой записан путь к файлу. Необходимо найти имя файла без расширения.
 def file(path):
@@ -22,3 +22,22 @@ def file(path):
     filename_parts = filename_with_extension.split('.')
     filename = filename_parts[0]
     return filename
+
+print("Выберите задачу для решения:")
+print("2: Найти все строчные символы латиницы")
+print("10: Найти количество задействованных символов латиницы в строке")
+print("17: Найти имя файла без расширения")
+
+choice = input("Введите номер задачи: ")
+if choice == "2":
+    string = input()
+    if find(string):
+        print("Строчные символы:",find(string))
+elif choice == "10":
+    string = input()
+    print("Количество задействованных символов латиницы в строке:", cou(string))
+elif choice == "17":
+    filename= input()
+    print("Имя файла без расширения:", file(filename))
+else:
+    print("Неверный выбор")
