6347464fdfcfa512f94e55b49668d1ca6d1112ff
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-16 11:40:57 +0300

    Update 5.py

--- 5.py
+++ 5.py
@@ -1,1 +1,10 @@
 #Дана строка. Необходимо найти все даты, которые описаны в виде "31 февраля 2007"
+import re
+def date(string):
+    pat = r"\d{1,2} (?:января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря) \d{4}"
+    dates = re.findall(pat, string)
+    return dates
+
+text = input("Введите текст")
+result = date(text)
+print(result)
