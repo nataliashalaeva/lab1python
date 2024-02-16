74c0f63a57250a2914b1ff75da3898be39e223be
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-02-16 11:57:52 +0300

    Update 6-8.py

--- 6-8.py
+++ 6-8.py
@@ -9,10 +9,10 @@
     
 #10 Дана строка. Необходимо найти количество задействованных символов латиницы в этой строке (без дубликатов).
 def cou(string):
-  chars = set()
-  for char in string:
-    if char.isalpha() and char.isascii() and char.islower():
-     chars.add(char)
+   chars = set()
+   for char in string:
+       if char.isalpha() and char.isascii() and char.islower():
+          chars.add(char)
   return len(chars)
 
 #17 Дана строка в которой записан путь к файлу. Необходимо найти имя файла без расширения.
