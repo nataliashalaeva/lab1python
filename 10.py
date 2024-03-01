e92cc8526556250a989f3fdebced4aee976bb849
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-01 11:49:09 +0300

    Update 10.py

--- 10.py
+++ 10.py
@@ -1,2 +1,12 @@
 #10 Дан список строк с клавиатуры. Упорядочить по количеству
 слов в строке.
+def f(string):
+    return len(string.split(" "))
+
+l = []
+s = input()
+l.append(s)
+while s != "": 
+    s = input()
+    if (s != ""): l.append(s)
+print(sorted(l, key=f))
