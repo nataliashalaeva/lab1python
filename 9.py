3006a4f74c165f910ff502aa73cf1f387be909e3
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-21 19:10:32 +0300

    Update 9.py

--- 9.py
+++ 9.py
@@ -1,8 +1,12 @@
 # Задание 9 Прочитать список строк с клавиатуры. Упорядочить по длине строки.
-l = []
-s = input()
-l.append(s)
-while s != "": 
-    s = input()
-    if (s != ""): l.append(s)
-print(sorted(l, key=len))
+
+strings = []
+print("Введите строки :")
+while True:
+    string = input()
+    if string == "":
+        break
+    strings.append(string)
+
+print("Отсортированный список строк по длине:")
+print(*sorted(strings, key=len), sep=" ")
