6a38bbc2e90c181bcef13549c7a8eb82684f70cc
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-24 16:25:05 +0300

    Update 15-19.py

--- 15-19.py
+++ 15-19.py
@@ -42,4 +42,44 @@ def func5(l1, l2):
         if i not in l1 and l2.count(i) == 1: l.append(i)
     return l
 
+num = input("Enter task number: ")
+while True:
+    if (num not in ["1","2","3","4","5"]): 
+        num = input("Enter task number: ")
+    else: break
 
+if num in ["1","3"]:
+    l = []
+    s = input()
+    l.append(int(s))
+    while s != "": 
+        s = input()
+        if (s != ""): l.append(int(s))
+    if num == "1": print(func1(l))
+    else: print(func3(l))
+    exit()
+elif num in ["2","4"]:
+    l = []
+    s = input()
+    l.append(int(s))
+    while s != "": 
+        s = input()
+        if (s != ""): l.append(int(s))
+    a = input()
+    b = input()
+    if num == "2": print(func2(l, int(a), int(b)))
+    else: print(func4(l, int(a), int(b)))
+else:
+    l1 = []
+    s = input()
+    l1.append(int(s))
+    while s != "": 
+        s = input()
+        if (s != ""): l1.append(int(s))
+    l2 = []
+    s = input()
+    l2.append(int(s))
+    while s != "": 
+        s = input()
+        if (s != ""): l2.append(int(s))
+    print(func5(l1,l2))
