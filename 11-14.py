99b6d9086d6666c28c28bc10109abef292ab676a
nataliashalaeva <112845670+nataliashalaeva@users.noreply.github.com>
2024-03-24 16:10:44 +0300

    Update 11-14.py

--- 11-14.py
+++ 11-14.py
@@ -9,5 +9,16 @@ def sort_by_quad_deviation(strings):
     return sorted(strings, key=lambda x: (sum((ord(c) - first_string_avg) ** 2 for c in x) / len(x)) ** 0.5)
     
 #7 В порядке увеличения разницы между количеством сочетаний «гласная-согласная» и «согласная-гласная» в строке.
+def count_vowel_consonant_combinations(string):
+    vowels = "aeiouAEIOU"
+    consonants = "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ"
+    vowel_consonant_count = 0
+    consonant_vowel_count = 0
+    for i in range(len(string) - 1):
+        if string[i] in vowels and string[i+1] in consonants:
+            vowel_consonant_count += 1
+        elif string[i] in consonants and string[i+1] in vowels:
+            consonant_vowel_count += 1
+    return abs(vowel_consonant_count - consonant_vowel_count)
 #11 В порядке квадратичного отклонения дисперсии максимального среднего веса ASCII-кода тройки символов в строке от максимального среднего веса ASCII-кода тройки символов в первой строке.
 
